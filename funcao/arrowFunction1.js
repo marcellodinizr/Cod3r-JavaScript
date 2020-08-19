6ade160004f7bf02e72f768ab823fdc169fd04d4
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-19 17:14:25 -0300

    arrow 2

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,20 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implícito
+
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'Ola'
+}
+
+ola = () => 'Ola'
+ola = _ => 'Ola' // possui parametro
+
+console.log(ola())
