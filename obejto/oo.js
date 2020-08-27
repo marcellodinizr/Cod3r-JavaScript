808039dc46905c66a0b3381264e57566dbe11994
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-27 17:27:18 -0300

    getter & setter

---
+++ obejto/oo.js
@@ -0,0 +1,16 @@
+// CÓDIGO NÃO EXECUTAVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ....
+    }
+}
+
+objeto.processamento() // Foco passou a ser o ojbeto
