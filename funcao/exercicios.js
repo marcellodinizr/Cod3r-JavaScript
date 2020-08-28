a1a1ce77c3ebf2b2cba0247d11ac8c240b768a13
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-28 17:08:02 -0300

    att

--- funcao/exercicios.js
+++ funcao/exercicios.js
@@ -481,3 +481,24 @@ function inverterVetor(vetorA, vetorB) {
 let vetorA = [1, 2, 3]
 let vetorB = [4, 5, 6]
 inverterVetor(vetorA, vetorB)
+
+// ATIVIDADE 40
+function conceituarNotas(notas) {
+    let conceitos = []
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 0 && notas[i] <= 4.9) {
+            conceitos.push('D')
+        } else if (notas[i] >= 5 && notas[i] <= 6.9) {
+            conceitos.push('C')
+        } else if (notas[i] >= 7 && notas[i] <= 8.9) {
+            conceitos.push('B')
+        } else if (notas[i] >= 9 && notas[i] <= 10) {
+            conceitos.push('A')
+        } else
+            conceitos.push('Invalido')
+    }
+    return conceitos
+}
+let notas = [10, 13, 9, 8.2, 2.5, 7.7, 6.8]
+let vetorConceitos = conceituarNotas(notas)
+console.log(vetorConceitos)
